49d3ea10e4806d10d9833a7b4e68c95b310c9c31
Your Name <you@example.com>
2023-08-20 19:49:11 +0100

    add project apps   😎

---
+++ js/login.js
@@ -0,0 +1,36 @@
+const typedTitle = new Typed(".multiple-title", {
+  strings: [
+    "● Les entreprises avec des employés hautement engagés et productifs ont un chiffre d affaires 21% plus élevé",
+
+    "● Les entreprises les plus productives ont une rentabilité 50% plus élevée",
+    "● Turner Construction, Skanska et Bechtel sont des exemples d'entreprises qui utilisent  le timesheet pour améliorer leur productivité.",
+  ],
+  typedSpeed: 100,
+  backspeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+
+const typedContent = new Typed(".multiple-content", {
+  strings: [
+    "● Le timesheet est un outil essentiel pour une meilleure gestion du temps et de la productivité dans une entreprise.",
+    "● Le suivi de la productivité fournit des données précieuses pour prendre des décisions éclairées.",
+    "● Sans suivi régulier de la productivité, il est difficile ​d identifier les inefficacités et les opportunités d'amélioration.",
+  ],
+  typedSpeed: 100,
+  backspeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+
+// const typedApp = new Typed(".multiple-logo", {
+//   strings: [
+//     "T  I  M  E  S  H  E  E  T",
+//     "● ● ● ● ● ● ●",
+//     "T  I  M  E  S  H  E  E  T",
+//   ],
+//   typedSpeed: 100,
+//   backspeed: 100,
+//   backDelay: 1000,
+//   loop: true,
+// });
